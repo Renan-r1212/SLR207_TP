8634e87c239f88d43a316eb87bc2c588cdf8cc09
Renan Rodrigues <renan.rodrigues@telecom-paristech.fr>
2020-03-02 16:55:07 +0100

    2.8 - Done

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/82/b08680de505a001a15b5f850cab8d7d0
@@ -0,0 +1,11 @@
+
+public class WordOccurrence {
+
+	public static void main(String[] args) {
+		FileWordCount x = new FileWordCount();
+		
+		x.countAndDisplay("input.txt");
+		
+	}
+
+}
--- Hadoop MapReduce/src/FileWordCount.java
+++ Hadoop MapReduce/src/FileWordCount.java
@@ -17,11 +17,17 @@ public class FileWordCount {
 	private int prevValue;
 	private Map<String, Integer> sortedWords;
 	private Map<String, Integer> sortedByCount;
+	private String outputFilePath;
+	private long startTime;
+	private long endTime;
+	private long totalTimeAlpha;
+	private long totalTimeOcc;
 	
 
 	public FileWordCount() {
 		wordsList = new ArrayList<String>();
 		wordOcc = new HashMap<String, Integer>();
+		outputFilePath = "/cal/homes/rrodrigues/Telecom_Paris/SLR/SLR207/SLR207_TP/Hadoop MapReduce/output.txt";
 	}
 	
 	public String fileToString(String file) {
@@ -44,34 +50,84 @@ public class FileWordCount {
 	
 	
 	public Map<String, Integer> sortByValue(final Map<String, Integer> wordCounts) {
-		sortedWords = new TreeMap<String, Integer>(wordCounts);
 		
-        return sortedWords.entrySet()
+        return wordCounts.entrySet()
                 .stream()
                 .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 	
 	
-	public void countAndDisplay(String file) {		
-		fileStr = fileToString(file);			
+	public void hashMapToTxt(final Map<String, Integer> sortedHashMap, long _totalTimeOcc, long _totalTimeAlpha) {
+        File file = new File(outputFilePath);
+        
+        BufferedWriter bf = null;;
+        
+        try{
+            
+            //create new BufferedWriter for the output file
+            bf = new BufferedWriter( new FileWriter(file) );
+ 
+            //iterate map entries
+            bf.write("Frequency count time: " + _totalTimeOcc);
+            bf.newLine();
+            bf.write("Alphabetical sort time: " + _totalTimeAlpha);
+            bf.newLine();
+            
+            for(String key : sortedHashMap.keySet()){
+                
+                //put key and value separated by a colon
+                bf.write( key + " " + sortedHashMap.get(key));
+                
+                //new line
+                bf.newLine();
+            }
+            
+            bf.flush();
+ 
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            
+            try{
+                //always close the writer
+                bf.close();
+            }catch(Exception e){}
+        }
+    }
+	
+	public void countAndDisplay(String file) {	
+		startTime = System.currentTimeMillis();
+		
+		fileStr = fileToString(file);
+		fileStr = fileStr.replaceAll("\\p{P}","");
 		words = fileStr.split(" ");
 		wordsList = Arrays.asList(words);
 		
 		wordIt = wordsList.listIterator();
 		while(wordIt.hasNext()){
 			itStr = wordIt.next();
-			if(wordOcc.containsKey(itStr)) {
-				prevValue = wordOcc.get(itStr);
-				wordOcc.replace(itStr, (prevValue + 1));				
-			}else {
-				wordOcc.put(itStr, 1);		
+			if(!itStr.isBlank()) {
+				itStr = itStr.toLowerCase();
+				if(wordOcc.containsKey(itStr)) {
+					prevValue = wordOcc.get(itStr);
+					wordOcc.replace(itStr, (prevValue + 1));				
+				}else {
+					wordOcc.put(itStr, 1);		
+				}
 			}
-		}	
-        sortedByCount = sortByValue(wordOcc);
-		
-		for(String key : sortedByCount.keySet()) {
-			System.out.println(key + " " + sortedWords.get(key));
 		}
+        endTime = System.currentTimeMillis();
+        totalTimeOcc = endTime - startTime;
+		
+		sortedWords = new TreeMap<String, Integer>(wordOcc);
+		
+        endTime = System.currentTimeMillis();
+        totalTimeAlpha = endTime - startTime;
+        
+        sortedByCount = sortByValue(sortedWords);		
+
+        hashMapToTxt(sortedByCount, totalTimeOcc, totalTimeAlpha);
+        System.out.print("Output generated");
 	}
 }
