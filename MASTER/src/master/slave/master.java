43983f18021447363c1412b9d18d5d5b9b1375c4
renan-rodrigues <renan.rodrigues.r1212@gmail.com>
2020-05-12 08:28:18 +0200

    SLR207 - TP - Étape 10.1 - 1.0

--- MASTER/src/master/slave/master.java
+++ MASTER/src/master/slave/master.java
@@ -1,20 +1,42 @@
 package master.slave;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class master {
 	private LinkedBlockingQueue<String> timeOutList;
+	private ArrayList<String> machinesList;
 	private ProcessBuilder pb;
 	private Process process;
 	private String output;
+	private BufferedReader read;
+	private String remoteMachine;
+	private InetAddress ip;
+	private String hostname;
+	private int splitFileNum;
 	
 	public master(){
-		timeOutList = new LinkedBlockingQueue<String>();
+		timeOutList  = new LinkedBlockingQueue<String>();
+		machinesList = new ArrayList<String>();
 		pb = new ProcessBuilder("java", "-jar", "/home/renan/Documents/telecom_paris/SLR/SLR207/SLR207_TP/SLAVE/slave.jar");
 		process = null;
 		output = null;
+		splitFileNum = 0;
+		
+		try {
+			ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+		} catch (UnknownHostException e){
+			e.printStackTrace();
+		}
 	}
+	
 	
 	public void outputReader() {
 		try {
@@ -46,10 +68,33 @@ public class master {
 		}
 	}
 	
+	public void deploySplits() {
+		try {
+			read = new BufferedReader(new FileReader("machines.txt"));
+			while((remoteMachine = read.readLine()) != null) {	
+				machinesList.add(remoteMachine);
+				
+				pb = new ProcessBuilder("ssh", "rrodrigues@" + remoteMachine, "mkdir -p /tmp/rrodrigues/splits",
+										"&& echo splits directory " + splitFileNum + " deployed",
+										"&& scp " + hostname +  ":/tmp/rrodrigues/splits/S"+ splitFileNum + ".txt", remoteMachine + ":/tmp/rrodrigues/splits/",
+										"&& hostname");	
+				pb.start();
+				splitFileNum++;
+			}
+			System.out.println("Splits deployed");
+			read.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		//% java -jar slave.jar 
 		
 		master m = new master();
-		m.outputReader();
+		m.deploySplits();
 	}
 }
